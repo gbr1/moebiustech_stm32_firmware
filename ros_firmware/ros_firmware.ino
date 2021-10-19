9256ac4990663480f209f8c6436f8fb8a1bc6848
gbr1 <giovannididio.bruno@gmail.com>
2021-10-19 12:46:55 +0200

    added iumu publisher, 30hz instead of 100hz reached

--- ros_firmware/ros_firmware.ino
+++ ros_firmware/ros_firmware.ino
@@ -1,9 +1,35 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2019 Giovanni di Dio Bruno https://gbr1.github.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "board_pins.h"
 #include "motorcontroller.h"
 #include <ros.h>
 #include <moebiustech_stm32_ros/Drive.h>
 #include <moebiustech_stm32_ros/Feedback.h>
 #include <moebiustech_stm32_ros/Status.h>
+#include <sensor_msgs/Imu.h>
+
 
 // ROS
 ros::NodeHandle  nh;
@@ -13,6 +39,7 @@ uint16 timer_joint=0;
 uint16 timer_led=0;
 uint16 timer_motors=0;
 uint16 timer_battery=0;
+uint16 timer_imu=0;
 
 
 bool led_status = false;
@@ -37,6 +64,10 @@ ros::Publisher jointstate("/moebiustech_stm32/feedback", &robot_state);
 moebiustech_stm32_ros::Status battery_state;
 ros::Publisher batterystate("/moebiustech_stm32/status", &battery_state);
 
+//publisher for imu
+sensor_msgs::Imu imu_msg;
+ros::Publisher imu_pub("/moebiustech_stm32/imu", &imu_msg);
+
 void setup() {
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,LOW);
@@ -51,6 +82,7 @@ void setup() {
   nh.subscribe(jointdrive);
   nh.advertise(jointstate);
   nh.advertise(batterystate);
+  nh.advertise(imu_pub);
 
   //1ms interrupt enabled
   systick_attach_callback(tick);
@@ -74,7 +106,8 @@ void loop() {
     updateLed();
     updateMotors();
     publishJoints();
-    updateBattery();    
+    updateBattery();
+    updateImu();    
     nh.spinOnce();
     //delay(1);
   }
@@ -148,9 +181,28 @@ void updateBattery(){
   }
 }
 
+void updateImu(){
+  if (timer_imu>=10){
+    imu_msg.header.frame_id="imu_link";
+    imu_msg.header.stamp=nh.now();
+
+
+
+
+
+
+    imu_pub.publish(&imu_msg);
+    timer_imu=0;
+  }
+  
+  
+}
+
+
 void tick(void){
   timer_led++;
   timer_joint++;
   timer_motors++;
   timer_battery++;
+  timer_imu++;
 }
